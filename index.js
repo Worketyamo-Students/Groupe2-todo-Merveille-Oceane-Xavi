6bf485375378e3b2e6f6fdca7be531ecf2cf972a
yotta11 <nguiyoumerveille@gmail.com>
2025-04-05 14:04:07 +0100

    fix:modification

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ mode.onclick = function () {
         //  categorie.style.background ="#25273D";
         parent.style.background = " #25273D";
         element.style.color = " #C8CBE7";
-        alltable.style.background= " #25273D"
-        activetable.style.background=" #25273D"
+        alltable.style.background = " #25273D"
+        activetable.style.background = " #25273D"
     } else {
         mode.src = "./assets/img/lune.svg";
         hero.style.backgroundImage = "url('./assets/img/image1.svg')";
@@ -37,10 +37,10 @@ mode.onclick = function () {
         foot.style.color = " #9495A5";
         check.style.borderColor = " #9495A5 "
         input.style.color = " #9495A5  "
-        alltable.style.background= "white"
-        activetable.style.background="white"
+        alltable.style.background = "white"
+        activetable.style.background = "white"
 
-        
+
     }
 
 }
@@ -59,7 +59,10 @@ let check1 = document.getElementById('check1')
 let checkimg1 = document.getElementById('checkimg1')
 let texte = document.getElementById('texte')
 let table1 = document.getElementById("table1")
-let alltable= document.getElementById("allTable")
+let table2 = document.getElementById("table2")
+let table3 = document.getElementById("table3")
+let alltable = document.getElementById("allTable")
+let compte=document.getElementById("compte")
 
 let category = document.getElementById("category")
 let count = 0;  //compteur
@@ -93,72 +96,131 @@ input.addEventListener('keydown', function (event) {
             let newElement = element.cloneNode(true);
             newElement.style.display = "flex";
             let textElement = newElement.querySelector('#texte');
-
+            count = count + 1;
+            compte.innerHTML= count +" "+ "items left"
+            console.log(count)
             if (textElement) {
                 textElement.innerHTML = contenu;
             }
             parent.appendChild(newElement);//ajout de l'element dans le tableau
             parent.insertBefore(newElement, categorie);
             checkimg.style.display = "none"
-            
+
             // faire apparaitre le bouton delete "mouseover"
             let part = newElement.querySelector('#sousElement')
             let select = part.querySelector('#select')
             let check1 = part.querySelector('#checkimg1')
             let sup = document.createElement('img')
-            sup.src="/assets/img/croix.svg"; sup.classList = "cursor-pointer" 
-            newElement.addEventListener('mouseover', () =>{
+            sup.src = "/assets/img/croix.svg"; sup.classList = "cursor-pointer"
+            newElement.addEventListener('mouseover', () => {
                 part.appendChild(sup)
             })
 
-             // pour supprimer 
+            // pour supprimer 
             sup.addEventListener('click', () => {
                 parent.removeChild(newElement)
             })
             // pour disparaitre le delete 
-            newElement.addEventListener('mouseleave', () =>{
+            newElement.addEventListener('mouseleave', () => {
                 part.removeChild(sup)
             })
             // implementation des selections
-            let active = true 
+            let active = true
             select.addEventListener('click', () => {
-                if (active){
-                    select.style.background = "linear-gradient(to bottom, #3710BD 0, #A42395 100% )" 
-                    check1.style.display = "flex" 
+                if (active) {
+                    select.style.background = "linear-gradient(to bottom, #3710BD 0, #A42395 100% )"
+                    check1.style.display = "flex"
                     textElement.style.textDecoration = "line-trough"
                     textElement.style.color = "#D1D2DA"
-                }else {
+                } else {
                     select.style.background = ""
-                    check1.style.display = "" 
-                    textElement.style.textDecoration = "" 
-                    textElement.style.color = "" 
-                }  
+                    check1.style.display = ""
+                    textElement.style.textDecoration = ""
+                    textElement.style.color = ""
+                }
             })
             active = !active
 
             //effacer le contenu de l'input
-         input.value="";
-    check.style.backgroundImage="none";
-    parent.style.display="flex"
-       
+            input.value = "";
+            check.style.backgroundImage = "none";
+            parent.style.display = "flex"
+
 
             //tableau all
             table1.addEventListener('click', (a) => {
                 let all = [''];
                 all = newElement.cloneNode(true);
-                //all.push(element)
+
                 console.log(all)
                 alltable.appendChild(all)
-                category.style.display="flex"
+                category.style.display = "flex"
 
-                 alltable.insertBefore(all, category);
-                alltable.style.display="flex"
-                // categorie.style.display="flex"
-                parent.style.display="none";
+                alltable.insertBefore(all, category);
+                alltable.style.display = "flex"
 
-        
+                parent.style.display = "none";
+
+
             })
-            //implementation de l'effet de survol
+
+
+            //cocher les elements
+        
+            select.addEventListener("click", (e) => {
+                checkimg1.style.backgroundImage = "url('./assets/img/vrai.svg')";
+                select.style.backgroundImage = "linear-gradient(to bottom, #3710BD 0, #A42395 100% )";
+                texte.style.textDecoration = "line-through";
+            })
+
+            //table actived
+            table2.addEventListener('click', (a) => {
+                let activetable = []
+                activetable = newElement.cloneNode(true);
+                console.log(activetable)
+                alltable.appendChild(activetable)
+                category.style.display = "flex"
+                alltable.insertBefore(activetable, category);
+                alltable.style.display = "flex"
+                 all =activetable
+               
+
+                //suppression des coches
+                if (test) {
+
+                    alltable.removeChild(active)
+
+                }
+
+            })
+
+            //table completed 
+            table3.addEventListener("click", () => {
+                parent.style.display = "none";
+                if (test) {
+                    let completed = []
+                    alltable.appendChild(completed)
+                    completed = test;
+                }
+
+            })
+
+
+
+            //decocher les elements
+            //      select.addEventListener("click" ,(e)=> {
+            //      checkimg.style.display = "none";
+            //      select.style.backgroundImage ="none";
+            //      texte.style.textDecoration ="none";
+
+            //  })
+
+            // parent.addEventListener( 'mouseleave' , function (e) {
+            // croix.style.display="flex"
+
+            // }
+
+
 
 
 
@@ -169,17 +231,5 @@ input.addEventListener('keydown', function (event) {
 
 })
 
-//cocher les elements
-//   check1.addEventListener("click" ,(e)=> {
-//     checkimg1.style.display = "flex";
-//     check1.style.backgroundImage = "linear-gradient(to bottom, #3710BD 0, #A42395 100% )";
-//     texte.style.textDecoration ="line-through";
 
-
-//
-// parent.addEventListener( 'mouseleave' , function (e) {
-// croix.style.display="flex"
-
-// }
-//   })
 
